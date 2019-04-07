7529b8dbaefe31442a7541f1d6e350f935f6865d
AttackerCyber007 <44440552+AttackerCyber007@users.noreply.github.com>
2019-04-07 23:33:17 +0700

    Update ToolsV5.sh

--- ToolsV5.sh
+++ ToolsV5.sh
@@ -93,26 +93,18 @@ sleep 1
 clear
 echo
 echo $pur"==================="$cy" ==================="
-echo $i"Ôûê"$pu"  AUTHOR  :  5H311_1NJ3C706  "$i"                   Ôûê"
-echo $i"Ôûê"$pu"  WhatsApp:  08994422616"$i"                Ôûê"
-echo $i"Ôûê"$pu"  GMAIL   :  HackerBaloch07@gmail.com"$i"    Ôûê"
-echo $i"Ôûê"$pu"  TEAM :  BLACK PHANTOM CYBER"$i"                Ôûê"
+echo $i"Ôûê"$pu"  AUTHOR  :  5H311_1NJ3C706  "$i"                   
+echo $i"Ôûê"$pu"  WhatsApp:  08994422616"$i"                
+echo $i"Ôûê"$pu"  GMAIL   :  HackerBaloch07@gmail.com"$i"    
+echo $i"Ôûê"$pu"  TEAM :  BLACK PHANTOM CYBER"$i"                
 echo $pur"==================="$cy" ==================="
 echo
 echo $cy"MeNu BRUTEPORCE NeWS:"
 echo $ku"["$me"1"$ku"]"$i" REPORT FB"
-echo $ku"["$me"2"$ku"]"$i" BRUTE PORCE"
-echo $ku"["$me"3"$ku"]"$i" BRUTE FORCE FB CRACKER"
-echo $ku"["$me"4"$ku"]"$i" MBF"
-echo $ku"["$me"5"$ku"]"$i" CLONING YAHOO"
-echo $ku"["$me"6"$ku"]"$i" OSIF"
-echo $ku"["$me"7"$ku"]"$i" PROFILEGUARD FB"
-echo $ku"["$me"8"$ku"]"$i" BOT REACTION FACEBOOK"
-echo $ku"["$me"9"$ku"]"$i" SPAM CALL"
-echo $ku"["$me"10"$ku"]"$i" SPAM WHATSHAPP"
-echo $ku"["$me"11"$ku"]"$i" YOUTUBE-DL"
-echo $ku"["$me"12"$ku"]"$i" LACAK LOKASI"
-echo $i"["$me"13"$i"]"$ku" EXIT"
+echo $ku"["$me"2"$ku"]"$i" BRUTE FORCE FB CRACKER"
+echo $ku"["$me"3"$ku"]"$i" MBF"
+echo $ku"["$me"4"$ku"]"$i" PROFILEGUARD FB"
+echo $i"["$me"5"$i"]"$ku" EXIT"
 echo
 echo $pur"Ôò¡ÔöÇ["$pur"Masukkan pilihan anda"$pur"]"
 read -p"Ôò¦ÔöÇ>> " pil
@@ -133,22 +125,12 @@ then
 clear
 figlet -f slant "W A I T"|lolcat
 sleep 1
-git clone https://github.com/Senitopeng/fbbrute.git
-cd fbbrute
-python2 jomblo.py
-fi
-
-if [ $pil = 3 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
 git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
 cd Fb-Cracker-v.3
 python2 crack.py
 fi
 
-if [ $pil = 4 ]
+if [ $pil = 3 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
@@ -158,30 +140,7 @@ cd multi-bruteforce-facebook
 python2 MBF.py
 fi
 
-if [ $pil = 5 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/wahyuandhika/YahooCloning.git
-cd YahooCloning
-pip2 install requests mechanize
-pip2 install requirements
-python2 cloning.py
-fi
-
-if [ $pil = 6 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/CiKu370/OSIF.git
-cd OSIF
-pip2 install -r requirements.txt
-python2 osif.py
-fi
-
-if [ $pil = 7 ]
+if [ $pil = 4 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
@@ -191,57 +150,7 @@ cd FBshield
 python2 guard.py
 fi
 
-if [ $pil = 8 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/edi-nasa12/ROBOT.git
-cd ROBOT
-python2 blackbot.py
-fi
-
-if [ $pil = 9 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/Aditya021/SpamCall
-cd SpamCall
-php SpamCall.php
-fi
-
-if [ $pil = 10 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/4L13199/LITESPAM.git
-cd LITESPAM
-sh LITESPAM.sh
-fi
-
-if [ $pil = 11 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/rahmadxyz/sub-bot.git
-cd sub-bot
-php sub-bot.php
-fi
-
-if [ $pil = 12 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
-git clone https://github.com/thelinuxchoice/locator.git
-cd locator
-bash locator.sh
-fi
-
-if [ $pil = 13 ]
+if [ $pil = 5 ]
 then
 clear
 figlet -f slant "W A I T"|Ulolcat
